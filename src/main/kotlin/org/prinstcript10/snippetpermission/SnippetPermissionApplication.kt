45d942709dc0aa4d04372d7c641f80ccc374f5c5
manuelhernandezcarrasco <manuel.hernandez@ing.austral.edu.ar>
2024-10-02 18:17:52 -0300

    initial commit

---
+++ src/main/kotlin/org/prinstcript10/snippetpermission/SnippetPermissionApplication.kt
@@ -0,0 +1,11 @@
+package org.prinstcript10.snippetpermission
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SnippetPermissionApplication
+
+fun main(args: Array<String>) {
+    runApplication<SnippetPermissionApplication>(*args)
+}
